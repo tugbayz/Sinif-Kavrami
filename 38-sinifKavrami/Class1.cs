25c7875b500b9219890938b5bab755a6030a9d8d
tugbayz <tugbaayaaz@hotmail.com>
2024-08-12 14:19:52 +0300

    Class kavramı
    Class nedir nasıl oluşturulur, property nedir nasıl yazılır

---
+++ 38-sinifKavrami/Class1.cs
@@ -0,0 +1,63 @@
+﻿
+namespace _38_sinifKavrami
+{
+    internal class Personel
+
+    {
+        // Bir sınıfın içinde fields, proporties, metot yer alabilir. Sınıf üyeleri( class members);
+
+        // Property (özellik)
+
+        // Property ' i oluşturmak için prop yazıp iki kere tab tuşuna basarız
+
+        // propfull tab tab
+
+        public string ad { get; set; }
+
+        private string _soyad; // private sınıf içinden erişileblir
+
+        public string Soyad
+        {
+            get { return _soyad; }
+            set { _soyad = value.ToUpper(); }
+        }
+
+
+        public int yas { get; set; }
+
+        public string unvan { get; set; }
+
+        public decimal maas { get; set; }
+
+        public decimal tabanMaas { get; set; }
+
+        public int cocukSayisi { get; set; }
+
+
+        public void MaasHesapla()
+        {
+            if(cocukSayisi==0)
+            {
+                maas = tabanMaas;
+
+            }
+            else if (cocukSayisi<3)
+            
+                maas = tabanMaas * 1.05m; //maaşının %5i kadar
+            
+
+            else if(cocukSayisi>=3)
+            
+                maas = tabanMaas * 1.10m;
+            
+        }
+
+        public void PersonelBilgileriniYaz()
+        {
+            MaasHesapla();
+
+            Console.WriteLine($"Adı: {ad}\n Soyad: {Soyad}\n Yaşı : {yas}\n unvan: {unvan}\n Maaşı: {maas}");
+        }
+
+    }
+}
---
+++ 38-sinifKavrami/Program.cs
@@ -0,0 +1,37 @@
+﻿
+
+
+// Instance alma (örnek alma)
+
+//new Random();
+
+// Perspnel sınıfından bir nesne (örnek) (nesne) (instance) oluşturdum.
+
+using _38_sinifKavrami;
+
+Personel calisan1 = new Personel(); // Personel veri tipindeki çalışan değişkeni Personel nesnesini tutuyır
+
+calisan1.ad = "Tuğba";
+calisan1.Soyad = "Yaz";
+calisan1.yas = 25;
+calisan1.unvan = "Yazılım Uzmanı";
+
+Console.WriteLine("Personel Bilgileri");
+Console.WriteLine($"Adı: {calisan1.ad}\nSoyad: {calisan1.Soyad}\nYaşı: {calisan1.yas}\n Unvanı: {calisan1.unvan}");
+
+//////////////////////////////////////
+Console.WriteLine(new string('*', 40));
+
+Personel calisan2 = new Personel();
+
+//calisan2.ad = "Bilge";
+//calisan2.Soyad = "Adam";
+//calisan2.yas = 30;
+//calisan2.unvan = "Yazılımcı";
+
+//Console.WriteLine("Personel Bilgileri");
+
+calisan2.PersonelBilgileriniYaz();
+
+
+// NOt: Erişim belirteci private olan bir fielda dışardan erişemeyiz.İçerisine veri atamak veya veri okumak i.in propertyleri kullanırız.
